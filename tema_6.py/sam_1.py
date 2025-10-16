77169dfbe2c91b05e4294f138683ea5883c5b9c4
dermhl <ivan_da_anya@mail.ru>
2025-10-16 23:26:16 +0500

    Файл удалён

--- tema_6.py/sam_1.py
+++
@@ -1,6 +0,0 @@
-data = input("Введите числа, разделённые пробелом: ")
-list_numbers = [int(x) for x in data.split()]
-tuple_numbers = tuple(list_numbers)
-
-print("Список:", list_numbers)
-print("Кортеж:", tuple_numbers)
